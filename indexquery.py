6baee0692583a66b1b0b3d18bb3f15c78e096b3e
Philip Jenvey <pjenvey@underboss.org>
2019-07-26 11:47:46 -0700

    fix the FORCE_INDEX. notes on order by

--- indexquery.py
+++ indexquery.py
@@ -10,14 +10,20 @@ DB = "sync"
 
 USERID = "475bbf0d-17a2-4f7b-ab8e-92d48cba7e1b"
 COLL = 100
+
+# returns 8100 latest
 MODIFIED = "2019-03-11T12:30:00.45Z"
 # returns 100 latest
-MODIFIED = "2019-04-19T10:19:56.0Z"
+#MODIFIED = "2019-04-19T10:19:56.0Z"
+
 # %f isn't right: spanner's nanoseconds, not microseconds
 FORMAT = "%Y-%m-%dT%H:%M:%S.%fZ"
 
 INCLUDE_TTL_IN_BSOLM = False
 INCLUDE_TTL_IN_BSOLM = True
+
+_FORCE_BSOLM_INDEX = False
+#_FORCE_BSOLM_INDEX = True
 
 def query(instance, db):
     spanner_client = spanner.Client()
@@ -70,6 +76,26 @@ ORDER BY
         print('AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA')
 
         # XXX: do I need ORDER BY here?
+
+        # This index is ordered by modified DESC
+        # We either sort by:
+        # Sorting::Index  -> sortindex (Python: + id desc)
+        # Sorting::Newest -> modified desc (Python: + id asc)
+        # Sorting::Oldest -> modified asc (Python: + id asc)
+        # Sorting::None
+        # only the Python is sorting by the additional id
+        # sort. syncstorage-rs inherited *not* doing this from the go
+        # version, which doesn't do it.
+        #
+        # Sort by index was only added recently:
+        # Date:   Sun Jul 15 23:40:54 2018 -0700
+        # https://github.com/mozilla-services/server-syncstorage/commit/a0f8117
+        # https://bugzilla.mozilla.org/show_bug.cgi?id=1464008
+        #
+        # https://cloud.google.com/spanner/docs/query-syntax
+        # "UNNEST destroys the order of elements in the input ARRAY. Use
+        # the optional WITH OFFSET clause to return a second column with
+        # the array element indexes (see below)."
         q = """\
 SELECT
   bso.userid,
@@ -127,7 +153,7 @@ SELECT
   bso.payload,
   bso.ttl
 FROM
-  bso
+  bso%s
 WHERE
   bso.userid = @userid
   AND bso.collection = @coll
@@ -137,7 +163,7 @@ WHERE
 ORDER BY
   bso.modified DESC,
   bso.id ASC
-"""
+""" % ('@{FORCE_INDEX=BsoLastModified}' if _FORCE_BSOLM_INDEX else '')
         params = dict(
             userid=USERID,
             coll=COLL,
